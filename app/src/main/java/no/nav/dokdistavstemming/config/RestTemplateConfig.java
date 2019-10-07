bb45f666e4ea80e24a4d5ceb3e95d41feb8931f7
Gebremedhin, Tsigab Angosom <tsigab.angosom.gebremedhin@nav.no>
2019-10-07 21:26:14 +0200

    MMA-3643 øker readtime for resttemplate

--- app/src/main/java/no/nav/dokdistavstemming/config/RestTemplateConfig.java
+++ app/src/main/java/no/nav/dokdistavstemming/config/RestTemplateConfig.java
@@ -13,26 +13,18 @@ import java.time.Duration;
 @Configuration
 public class RestTemplateConfig {
 
-	public static final Duration DURATION_READTIMEOUT = Duration.ofMillis(60000L);
+	public static final Duration DURATION_READTIMEOUT = Duration.ofMillis(120000L);
 	public static final Duration DURATION = Duration.ofMillis(30000l);
 
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder, final ServiceuserAlias serviceuserAlias) {
-
-
-		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-		factory.setConnectTimeout(3 * 1000);
-		factory.setReadTimeout(7 * 10000);
-
 		RestTemplate restTemplate = restTemplateBuilder
 				.interceptors(new CallIdInterceptor())
 				.setReadTimeout(DURATION_READTIMEOUT)
 				.setConnectTimeout(DURATION)
 				.basicAuthentication(serviceuserAlias.getUsername(), serviceuserAlias.getPassword())
 				.build();
-		restTemplate.setRequestFactory(factory);
-
 		return restTemplate;
 	}
 
