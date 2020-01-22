73fffad5ba09ebcc5cec84c2bc0c0a92d385243c
Tsigab Angosom Gebremedhin <Tsigab.Angosom.Gebremedhin@nav.no>
2020-01-22 15:18:32 +0100

    skrudde-på OppretteJiraSak metoden.

--- app/src/main/java/no/nav/dokdistavstemming/scheduler/AvstemForsendelseScheduleConfig.java
+++ app/src/main/java/no/nav/dokdistavstemming/scheduler/AvstemForsendelseScheduleConfig.java
@@ -1,7 +1,6 @@
 package no.nav.dokdistavstemming.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.dokdistavstemming.domain.DistribusjonKanalCode;
 import no.nav.dokdistavstemming.service.serviceimp.AvstemForsendelseService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
-
-import java.util.Arrays;
 
 
 /**
@@ -22,34 +19,33 @@ import java.util.Arrays;
 @Slf4j
 public class AvstemForsendelseScheduleConfig implements SchedulingConfigurer {
 
-	private static final int POOL_SIZE = 4;
+    private static final int POOL_SIZE = 4;
 
-	private final String cronSchedule;
-	private final AvstemForsendelseService avstemForsendelseService;
+    private final String cronSchedule;
+    private final AvstemForsendelseService avstemForsendelseService;
 
 
-	public AvstemForsendelseScheduleConfig(@Value("${scheduler_interval_cron}") String cronScheduler,
-										   AvstemForsendelseService avstemForsendelseService) {
-		this.cronSchedule = cronScheduler;
-		this.avstemForsendelseService = avstemForsendelseService;
+    public AvstemForsendelseScheduleConfig(@Value("${scheduler_interval_cron}") String cronScheduler,
+                                           AvstemForsendelseService avstemForsendelseService) {
+        this.cronSchedule = cronScheduler;
+        this.avstemForsendelseService = avstemForsendelseService;
 
-	}
+    }
 
-	@Override
-	public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
 
-		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
 
-		taskScheduler.setPoolSize(POOL_SIZE);
-		taskScheduler.setThreadNamePrefix("dokdistavstemming-scheduled-task-pool-");
-		taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
-		taskScheduler.initialize();
+        taskScheduler.setPoolSize(POOL_SIZE);
+        taskScheduler.setThreadNamePrefix("dokdistavstemming-scheduled-task-pool-");
+        taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
+        taskScheduler.initialize();
 
-		scheduledTaskRegistrar.setTaskScheduler(taskScheduler);
-		scheduledTaskRegistrar.addCronTask(() -> Arrays.stream(DistribusjonKanalCode.values())
-				.forEach( kanal->
-						avstemForsendelseService.getMappedAvstemmForsendelseByDistKanal(kanal.name())), cronSchedule);
-	}
+        scheduledTaskRegistrar.setTaskScheduler(taskScheduler);
+        scheduledTaskRegistrar.addCronTask(() ->
+                avstemForsendelseService.oppretteAvstemmingForsendelseJiraSakByDistribusjonKanal(), cronSchedule);
+    }
 
 
 }
