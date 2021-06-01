ccf8deca8dd28964a6934f31254d0be209510d81
Tsigab Gebremedhin <tsigab.angosom@gmail.com>
2021-06-01 15:13:45 +0200

    MMA-5414 MMA-5433 rettet miljø path etter deploy feil

---
+++ app/src/main/java/no/nav/dokdistavstemming/security/SecurityRestConfig.java
@@ -0,0 +1,25 @@
+package no.nav.dokdistavstemming.security;
+
+import no.nav.freg.security.oidc.auth.common.HttpSecurityConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityRestConfig {
+
+
+    @Bean
+    public HttpSecurityConfigurer disableCsrfConfigurer() {
+        return new HttpSecurityConfigurer() {
+            @Override
+            public void configure(HttpSecurity http) throws Exception {
+                http.csrf().disable();
+            }
+        };
+    }
+
+
+}
