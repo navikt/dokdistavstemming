522ab4124ba4ff20c27b5d23758566cef7ebc7cb
tsigab <tsigab.angosom@gmail.com>
2022-11-16 09:36:19 +0100

    MMA-6409 samlet config klasser i Application.java

--- app/src/main/java/no/nav/dokdistavstemming/Application.java
+++ app/src/main/java/no/nav/dokdistavstemming/Application.java
@@ -1,10 +1,26 @@
 package no.nav.dokdistavstemming;
 
+import no.nav.dokdistavstemming.azure.AzureConfig;
+import no.nav.dokdistavstemming.config.AvstemForsendelseConfig;
+import no.nav.dokdistavstemming.config.DokdistavstemmingProperties;
+import no.nav.dokdistavstemming.config.WebClientConfig;
+import no.nav.dokdistavstemming.scheduler.Sdist002ScheduleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Import({
+
+@EnableScheduling
+@EnableConfigurationProperties(value = {
+		DokdistavstemmingProperties.class,
+		AzureConfig.class
+})
+@Import(value = {
+		WebClientConfig.class,
+		Sdist002ScheduleConfig.class,
+		AvstemForsendelseConfig.class,
 		CoreConfig.class,
 		Sdist004Config.class
 })
--- core/src/main/java/no/nav/dokdistavstemming/CoreConfig.java
+++ core/src/main/java/no/nav/dokdistavstemming/CoreConfig.java
@@ -1,37 +1,14 @@
 package no.nav.dokdistavstemming;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import no.nav.dokdistavstemming.azure.AzureConfig;
-import no.nav.dokdistavstemming.config.AvstemForsendelseConfig;
-import no.nav.dokdistavstemming.config.DokdistavstemmingProperties;
-import no.nav.dokdistavstemming.config.WebClientConfig;
 import no.nav.dokdistavstemming.metrics.DokMonitoringAspect;
-import no.nav.dokdistavstemming.scheduler.Sdist002ScheduleConfig;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableRetry
-@EnableScheduling
-@EnableConfigurationProperties(value = {
-		DokdistavstemmingProperties.class,
-		AzureConfig.class
-})
-@EnableAspectJAutoProxy
-@Import(value = {
-		WebClientConfig.class,
-		Sdist002ScheduleConfig.class,
-		AvstemForsendelseConfig.class
-})
-@ComponentScan(basePackages = "no.nav.dokdistavstemming")
 public class CoreConfig {
-
 	@Bean
 	public DokMonitoringAspect dokMonitoringAspect(MeterRegistry meterRegistry) {
 		return new DokMonitoringAspect(meterRegistry);
