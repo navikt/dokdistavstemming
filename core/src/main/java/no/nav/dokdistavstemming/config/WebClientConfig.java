06cb4b51c324fa67c26ab132eac8244a09f1ae87
tsigab <tsigab.angosom@gmail.com>
2022-11-21 10:12:55 +0100

    MMA-6409 økte webclient buffer size

--- core/src/main/java/no/nav/dokdistavstemming/config/WebClientConfig.java
+++ core/src/main/java/no/nav/dokdistavstemming/config/WebClientConfig.java
@@ -10,12 +10,15 @@ import reactor.netty.http.client.HttpClient;
 @Configuration
 public class WebClientConfig {
 
+	private static final int MAX_BUFFER_SIZE = 16 * 1024 * 1024;
+
 	@Bean
 	@Primary
 	public WebClient webClient(WebClient.Builder webClientBuilder) {
 		return webClientBuilder
 				.clone()
 				.clientConnector(new ReactorClientHttpConnector(httpClient()))
+				.codecs(clientCodec -> clientCodec.defaultCodecs().maxInMemorySize(MAX_BUFFER_SIZE))
 				.build();
 	}
 
