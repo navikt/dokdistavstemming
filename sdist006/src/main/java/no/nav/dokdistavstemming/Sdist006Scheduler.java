5d652f373e1b0ee53c28bd14abb3c4797174dc83
Joakim Borgersen <joakim.borgersen@nav.no>
2023-08-23 10:27:23 +0200

    fix scheduled

--- sdist006/src/main/java/no/nav/dokdistavstemming/Sdist006Scheduler.java
+++ sdist006/src/main/java/no/nav/dokdistavstemming/Sdist006Scheduler.java
@@ -20,7 +20,8 @@ public class Sdist006Scheduler {
 		this.sendUlesteForsendelserTilSentralPrintService = sendUlesteForsendelserTilSentralPrintService;
 	}
 
-	@Scheduled(cron = "${sdist006.cron.job}")
+	//kjør en gang 30s etter oppstart for å få ut logger
+	@Scheduled(initialDelay = 1000 * 30, fixedDelay=Long.MAX_VALUE) //cron = "${sdist006.cron.job}")
 	public void runSdist006() {
 		if (leaderElection.isLeader()) {
 			log.info("Starter sdist006 cron-jobb");
