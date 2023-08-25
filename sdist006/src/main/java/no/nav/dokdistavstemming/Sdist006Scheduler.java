83245d60a12593b2db6ad43e345c494f41eae397
Joakim Borgersen <joakim.borgersen@nav.no>
2023-08-25 10:59:33 +0200

    update sdist006 schedulering

--- sdist006/src/main/java/no/nav/dokdistavstemming/Sdist006Scheduler.java
+++ sdist006/src/main/java/no/nav/dokdistavstemming/Sdist006Scheduler.java
@@ -20,9 +20,10 @@ public class Sdist006Scheduler {
 		this.sendUlesteForsendelserTilSentralPrintService = sendUlesteForsendelserTilSentralPrintService;
 	}
 
-	//kjør en gang 30s etter oppstart for å få ut logger
-	@Scheduled(initialDelay = 1000 * 30, fixedDelay=Long.MAX_VALUE) //cron = "${sdist006.cron.job}")
+	//kjør en gang 90s etter oppstart for å få ut logger
+	@Scheduled(initialDelay = 1000 * 90, fixedDelay=Long.MAX_VALUE) //cron = "${sdist006.cron.job}")
 	public void runSdist006() {
+		log.info("runSdist006");
 		if (leaderElection.isLeader()) {
 			log.info("Starter sdist006 cron-jobb");
 			poolTaskExecutor.execute(sendUlesteForsendelserTilSentralPrintService::sendUlesteForsendelserTilSentralPrint);
