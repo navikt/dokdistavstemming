02ed36e0eddedd3ad629364786647b859b00eea5
Joakim Borgersen <joakim.borgersen@nav.no>
2023-08-25 09:37:35 +0200

    Koble til mq uten cert

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -23,4 +23,24 @@ then
     export  serviceuser_password=$(cat /var/run/secrets/nais.io/srvdokdistavstemming/password)
 fi
 
+if test -f /var/run/secrets/nais.io/certificate/srvdokdistavstemming/keystore
+then
+    echo "Setting DOKDISTAVSTEMMINGCERT_KEYSTORE"
+    CERT_PATH='/var/run/secrets/nais.io/certificate/srvdokdistavstemming/keystore-extracted'
+    openssl base64 -d -A -in /var/run/secrets/nais.io/certificate/srvdokdistavstemming/keystore -out $CERT_PATH
+    export DOKDISTAVSTEMMINGCERT_KEYSTORE=$CERT_PATH
+fi
+
+if test -f /var/run/secrets/nais.io/certificate/srvdokdistavstemming/keystorealias
+then
+    echo "Setting DOKDISTAVSTEMMINGCERT_KEYSTOREALIAS"
+    export DOKDISTAVSTEMMINGCERT_KEYSTOREALIAS=$(cat /var/run/secrets/nais.io/certificate/srvdokdistavstemming/keystorealias)
+fi
+
+if test -f /var/run/secrets/nais.io/certificate/srvdokdistavstemming/keystorepassword
+then
+    echo "Setting DOKDISTAVSTEMMINGCERT_PASSWORD"
+    export DOKDISTAVSTEMMINGCERT_PASSWORD=$(cat /var/run/secrets/nais.io/certificate/srvdokdistavstemming/keystorepassword)
+fi
+
 
