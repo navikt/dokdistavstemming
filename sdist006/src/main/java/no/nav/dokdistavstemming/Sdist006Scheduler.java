ea500e947157eb125dc754989a56e5153919722c
Joakim Borgersen <joakim.borgersen@nav.no>
2023-09-19 15:18:56 +0200

    dummy-commit for deploy

--- sdist006/src/main/java/no/nav/dokdistavstemming/Sdist006Scheduler.java
+++ sdist006/src/main/java/no/nav/dokdistavstemming/Sdist006Scheduler.java
@@ -21,7 +21,7 @@ public class Sdist006Scheduler {
 	}
 
 	//kjør en gang 60s etter oppstart for å få ut logger
-	@Scheduled(initialDelay = 1000 * 60, fixedDelay = Long.MAX_VALUE) //cron = "${sdist006.cron.job}")
+	@Scheduled(initialDelay = 1000 * 55, fixedDelay = Long.MAX_VALUE) //cron = "${sdist006.cron.job}")
 	public void runSdist006() {
 		if (leaderElection.isLeader()) {
 			log.info("Starter sdist006 cron-jobb");
