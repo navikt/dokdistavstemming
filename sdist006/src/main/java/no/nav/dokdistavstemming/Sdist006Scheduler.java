c5e8a3f7cc7a85ccd29bfb8458a69bb9b9a4ace8
Joakim Borgersen <joakim.borgersen@nav.no>
2024-06-26 15:31:58 +0200

    Add batch_id som mdc-verdi for å holde styr på kjøringene

--- sdist006/src/main/java/no/nav/dokdistavstemming/Sdist006Scheduler.java
+++ sdist006/src/main/java/no/nav/dokdistavstemming/Sdist006Scheduler.java
@@ -2,9 +2,16 @@ package no.nav.dokdistavstemming;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.dokdistavstemming.consumer.leaderelection.LeaderElectionConsumer;
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import static no.nav.dokdistavstemming.constants.MDCConstants.MDC_BATCh_ID;
 
 @Slf4j
 @Configuration
@@ -13,6 +20,7 @@ public class Sdist006Scheduler {
 	private final LeaderElectionConsumer leaderElection;
 	private final ThreadPoolTaskExecutor poolTaskExecutor;
 	private final SendUlesteForsendelserTilSentralPrintService sendUlesteForsendelserTilSentralPrintService;
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "dd.MM.HH.mm" );
 
 	public Sdist006Scheduler(ThreadPoolTaskExecutor poolTaskExecutor, LeaderElectionConsumer leaderElection, SendUlesteForsendelserTilSentralPrintService sendUlesteForsendelserTilSentralPrintService) {
 		this.leaderElection = leaderElection;
@@ -23,8 +31,13 @@ public class Sdist006Scheduler {
 	@Scheduled(cron = "${sdist006.cron.job}")
 	public void runSdist006() {
 		if (leaderElection.isLeader()) {
-			log.info("Starter sdist006 cron-jobb");
-			poolTaskExecutor.execute(sendUlesteForsendelserTilSentralPrintService::sendUlesteForsendelserTilSentralPrint);
+			try {
+				MDC.put(MDC_BATCh_ID, LocalDateTime.now().format(formatter));
+				log.info("Starter sdist006 cron-jobb");
+				poolTaskExecutor.execute(sendUlesteForsendelserTilSentralPrintService::sendUlesteForsendelserTilSentralPrint);
+			} finally {
+				MDC.clear();
+			}
 		}
 	}
 }
